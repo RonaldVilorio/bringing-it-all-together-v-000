df2d5a0b0de718680df28838bf6102e5585d4e6b
ronald vilorio <soccer23415@gmail.com>
2018-01-06 00:27:01 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,7 @@ class Dog
   attr_accessor :name,:breed,:id
 
   def initialize(attributes)
-    # attributes.each{|key,value| self.send("#{key}=",value)}
+    attributes.each{|key,value| self.send("#{key}=",value)}
     # @name = name
     # @breed = breed
     # @id = id
