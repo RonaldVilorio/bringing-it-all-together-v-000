d1dd60168bbb6b810c4d6f7904755209a981fc4f
ronald vilorio <soccer23415@gmail.com>
2018-01-06 01:17:19 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -23,9 +23,9 @@ class Dog
   def self.new_from_db(row)
     binding.pry
 
-    new_dog.id = row[0]
-    new_dog.name = row[1]
-    new_dog.grade = row[2]
+    id = row[0]
+    name = row[1]
+    breed = row[2]
     new_dog = self.new(row)
     new_dog
 
