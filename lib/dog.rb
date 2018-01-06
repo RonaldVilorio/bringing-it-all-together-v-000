1c718eba9460bbaa9e5ab6d5092a88998949bc4d
ronald vilorio <soccer23415@gmail.com>
2018-01-06 00:17:59 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,64 @@
+class Dog
+  attr_accessor :name,:breed,:id
+
+  def initialize(attributes)
+    attributes.map{|key,value| self.send("#{key}=",value)}
+  end
+  
+  def self.create_table
+    sql = "CREATE TABLE IF NOT EXISTS dogs(
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      breed TEXT
+    )"
+    DB[:conn].execute(sql)
+  end
+  def self.drop_table
+    sql = "DROP table IF EXISTS dogs"
+    DB[:conn].execute(sql)
+  end
+  def self.new_from_db(row)
+    binding.pry
+    new_dog = self.new
+    new_dog.id = row[0]
+    new_dog.name = row[1]
+    new_dog.grade = row[2]
+    new_do
+
+  end
+  def self.find_by_name(name)
+    sql = "SELECT * FROM dogs WHERE name = ?"
+    DB[:conn].execute(sql,name).map{|row| self.new_from_db(row)}
+
+  end
+  def update
+    sql = "UPDATE dogs SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql,@name,@grade,@id)
+  end
+
+  def save
+    if @id
+      self.update
+    else
+      sql = "INSERT INTO dogs (name,breed) VALUES (?,?)"
+      DB[:conn].execute(sql,@name,@breed)
+
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+      self
+    end
+  end
+
+  def self.create(hash)
+    new_dog = self.new(hash)
+    new_dog.save
+  end
+
+  # def self.find_by_id(id)
+  #   # binding.pry
+  #   sql = "SELECT * FROM dogs WHERE id = ?"
+  #   DB[:conn].execute(sql,id).map{|row| self.new(row)}
+  # 
+  # end
+
+
+end
