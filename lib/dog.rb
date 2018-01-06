727785711e2e1e6a91771851a588a7ccdd2522c1
ronald vilorio <soccer23415@gmail.com>
2018-01-06 01:16:58 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -22,13 +22,15 @@ class Dog
   end
   def self.new_from_db(row)
     binding.pry
-    new_dog = self.new(row)
+
     new_dog.id = row[0]
     new_dog.name = row[1]
     new_dog.grade = row[2]
+    new_dog = self.new(row)
     new_dog
 
   end
+
   def self.find_by_name(name)
     sql = "SELECT * FROM dogs WHERE name = ?"
     DB[:conn].execute(sql,name).map{|row| self.new_from_db(row)}
